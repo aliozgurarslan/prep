92198c92522afaf93beab8cf75b3e82e288639cd
aliozgurarslan <117865292+aliozgurarslan@users.noreply.github.com>
2024-07-28 13:20:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ new Vue({
                 this.correctItems.push(...this.selectedItems);
                 this.wrongGuessMessage = "";
                 if (this.correctItems.length === this.items.length) {
-                    this.successMessage = "Tebrikler! Bütün grupları bildiniz!";
+                    this.successMessage = "Tebrikler! Duvarı yendiniz! Her gün yeni bir duvar.";
                     this.storeGameState();
                 }
             } else {
